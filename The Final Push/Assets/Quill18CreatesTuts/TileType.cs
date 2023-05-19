99ea741578d4ba06c6e333df1e8e3e2cd664e932
Simon Henderson <simonhenderson100@gmail.com>
2023-05-19 09:42:33 -0700

    Finished the Pathfinding system
    Finished with Quill18's tutorials. It wasn't exactly what I wanted, especially at the end (being closer to Civ movement than FE movement) but I think I can make it work.

--- The Final Push/Assets/Quill18CreatesTuts/TileType.cs
+++ The Final Push/Assets/Quill18CreatesTuts/TileType.cs
@@ -8,5 +8,6 @@ public class TileType
     public string name;
     public GameObject tileVisualPrefab;
 
+    public float movementCost = 1;
     public bool isWalkable = true;
 }
--- The Final Push/Assets/Quill18CreatesTuts/Unit.cs
+++ The Final Push/Assets/Quill18CreatesTuts/Unit.cs
@@ -6,6 +6,47 @@ public class Unit : MonoBehaviour
 {
     public int tileX;
     public int tileY;
+    public TileMap map;
 
-    public List<TileMap.Node> currentPath = null;
+    public List<Node> currentPath = null;
+
+    void Update()
+    {
+        if (currentPath != null)
+        {
+            int curNode = 0;
+
+            while (curNode < currentPath.Count - 1)
+            {
+                Vector3 start = map.TileCoordToWorldCoord(currentPath[curNode].x, currentPath[curNode].y) + new Vector3 (0,0,-1f);
+                Vector3 end = map.TileCoordToWorldCoord(currentPath[curNode + 1].x, currentPath[curNode + 1].y) + new Vector3 (0,0,-1f);
+
+                Debug.DrawLine(start, end, Color.red);
+
+                curNode++;
+            }
+        }
+    }
+
+    public void MoveNextTile()
+    {
+        if (currentPath == null)
+        {
+            return;
+        }
+
+        while (currentPath != null)
+        {
+            currentPath.RemoveAt(0);
+
+            tileX = currentPath[0].x;
+            tileY = currentPath[0].y;
+            transform.position = map.TileCoordToWorldCoord(tileX, tileY);
+
+            if (currentPath.Count == 1)
+            {
+                currentPath = null;
+            }
+        }
+    }
 }
