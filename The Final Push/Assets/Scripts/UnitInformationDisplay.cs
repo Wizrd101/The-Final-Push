1cc48e1e724144ce668e92c91882e303be61c498
Simon Henderson <simonhenderson100@gmail.com>
2023-06-05 10:29:51 -0700

    End of Day Push

--- The Final Push/Assets/Scripts/UnitInformationDisplay.cs
+++ The Final Push/Assets/Scripts/UnitInformationDisplay.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UnitInformationDisplay : MonoBehaviour
 {
     Canvas dispCanvas;
 
+    TextMeshProUGUI titleText;
+
     RaycastHit2D hit;
+
+    LayerMask unitLayers;
 
     void Awake()
     {
         dispCanvas = GetComponent<Canvas>();
+
+        titleText = dispCanvas.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
 
         Physics2D.queriesHitTriggers = true;
     }
@@ -19,21 +26,27 @@ public class UnitInformationDisplay : MonoBehaviour
     void Start()
     {
         dispCanvas.enabled = false;
+
+        unitLayers = LayerMask.GetMask("PlayerUnit", "EnemyUnit");
     }
 
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        //Debug.Log("hit outputs: " + Camera.main.transform.position + " " + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        Vector3 exPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, new Vector3(exPos.x, exPos.y, 0f), Mathf.Infinity, unitLayers);
+        //Debug.Log("hit outputs: " + Camera.main.transform.position + " " + new Vector3(exPos.x, exPos.y, 0f));
+        Debug.DrawLine(Camera.main.transform.position, new Vector3(exPos.x, exPos.y, 0f), Color.blue, 0.5f);
         if (hit.collider)
         {
+            Debug.Log(hit.collider.gameObject.name);
             Debug.Log("Triggered display");
             dispCanvas.enabled = true;
+            titleText.text = hit.collider.gameObject.name;
 
         }
         else
         {
-
+            Debug.Log("Untriggered display");
             dispCanvas.enabled = false;
         }
     }
